096eba22b31efeb686c0c0cf19aa727cb86f138d
Joaquin Viera <joaquin.viera@outlook.com>
2023-04-03 21:48:57 -0300

    useFetch hook

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export function useFetch(url) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError(err);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+  return { data, loading, error };
+}
