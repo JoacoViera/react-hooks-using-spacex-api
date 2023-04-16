3977ab6d80cf37b9adf57b3dab9f72bd17833e4f
Joaquin Viera <joaquin.viera@outlook.com>
2023-04-15 23:51:42 -0300

    new loader

--- src/Pages/Loader/index.tsx
+++ src/Pages/Loader/index.tsx
@@ -1,7 +1,30 @@
 import './styles.css';
 
 const Loader = (): JSX.Element => {
-  return <div className="loader" />;
+  return (
+    <>
+      <div className="jelly"></div>
+
+      <svg width="0" height="0" className="jelly-maker">
+        <defs>
+          <filter id="uib-jelly-ooze">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="6.25"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+              result="ooze"
+            />
+            <feBlend in="SourceGraphic" in2="ooze" />
+          </filter>
+        </defs>
+      </svg>
+    </>
+  );
 };
 
 export default Loader;
