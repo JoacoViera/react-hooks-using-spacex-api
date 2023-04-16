9029d80b03e853b87efe7e9186254890098c5b34
Joaquin Viera <joaquin.viera@outlook.com>
2023-04-15 23:53:13 -0300

    render rockets

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import './App.css';
+import Card from './components/Card';
 import useFetch from './hooks/useFetch';
 import ErrorPage from './Pages/ErrorPage';
 
 import Loader from './Pages/Loader';
 
+interface Rocket {
+  id: string;
+  name: string;
+  description: string;
+  flickr_images: string[];
+}
+
 const App = (): JSX.Element => {
   const { error, loading, data } = useFetch(
-    'https://api.spacexdata.com/v4/rockets'
+    'https://api.spacexdata.com/v4/rockets?limit=10&offset=10'
   );
 
-  console.log('request', { data, error, loading });
+  console.log('request', data);
 
   if (loading === true) {
     return <Loader />;
@@ -20,8 +28,15 @@ const App = (): JSX.Element => {
   }
 
   return (
-    <div className="App">
-      <h1>Hello Word</h1>
+    <div className="App flex flex-wrap justify-center">
+      {data.map((rocket: Rocket) => (
+        <Card
+          key={rocket.id}
+          name={rocket.name}
+          description={rocket.description}
+          image={rocket?.flickr_images[0]}
+        />
+      ))}
     </div>
   );
 };
